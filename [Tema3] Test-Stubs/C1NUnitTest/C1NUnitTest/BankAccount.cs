a4786aa496a4765283c4f1a81225262c35b4cec5
pescaru98 <mihai.pescaru@elysian-software.com>
2020-12-02 21:54:53 +0200

    Tema3 - Stubs

---
+++ [Tema3] Test-Stubs/C1NUnitTest/C1NUnitTest/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using C1NUnitTest.CurrencyConverter;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C1NUnitTest
+{
+    public class BankAccount
+    {
+        //Balance -> [0,10000]
+        public double Balance { get; set; }
+        public static double MIN_BALANCE { get; } = 0.0;
+        public static double MAX_BALANCE { get; } = 10000.0;
+
+        public BankAccount()
+        {
+            this.Balance = 0;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount < 0) throw new NegativeNumberException("You provided a negative number");
+            if (this.Balance + amount > MAX_BALANCE) throw new TooManyFundsException("Exceeded limit "+MAX_BALANCE);
+            this.Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount < 0) throw new NegativeNumberException("You provided a negative number");
+            if (this.Balance - amount < MIN_BALANCE) throw new NotEnoughFundsException("Not enough funds!");
+            this.Balance -= amount;
+        }
+
+        public void Transfer(BankAccount destination, double amount)
+        {
+            destination.Deposit(amount);
+            this.Withdraw(amount);
+        }
+
+        public void TransferFromEurToRon(BankAccount destination, double amountInEur, ICurrencyConverter converter)
+        {
+            destination.Deposit(converter.convertFromEurToRon(amountInEur));
+            this.Withdraw(converter.convertFromEurToRon(amountInEur));
+        }
+    }
+}
---
+++ [Tema3] Test-Stubs/NUnitTestingC1/NUnitTestingC1/BankAccountTesting.cs
@@ -0,0 +1,47 @@
+﻿using C1NUnitTest;
+using C1NUnitTest.CurrencyConverter;
+using NUnit.Framework;
+
+namespace NUnitTestingC1
+{
+    [TestFixture]
+    class BankAccountTesting
+    {
+        BankAccount Source;
+        BankAccount Destination;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            //arrange
+            Source = new BankAccount();
+            Source.Deposit(5000);
+            Destination = new BankAccount();
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(1500)]
+        [TestCase(-1)]
+        public void TransferInEur(int amountInEur)
+        {
+            //arrange
+            ICurrencyConverter converter = new CurrencyConverterStub();
+            double initialBalance = Source.Balance;
+            //act
+            Source.TransferFromEurToRon(Destination, amountInEur, converter);
+            //assert
+            Assert.AreEqual(initialBalance - converter.convertFromEurToRon(amountInEur),Source.Balance);
+
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            Source = null;
+            Destination = null;
+            System.Console.WriteLine("Finished test");
+        }
+    }
+}
